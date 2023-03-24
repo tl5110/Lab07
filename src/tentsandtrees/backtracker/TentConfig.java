d72da5c64f61d9f2244ab45c698be66a6d4c8385
Tiffany Lee <tl5110@g.rit.edu>
2023-03-24 19:01:33 -0400

    Tested and passed testSuccessors1() and testSuccessors2()

--- src/tentsandtrees/backtracker/TentConfig.java
+++ src/tentsandtrees/backtracker/TentConfig.java
@@ -12,16 +12,21 @@ import java.util.ArrayList;
  *  Configuration methods necessary for the Backtracker solver.
  *
  *  @author RIT CS
- *  @author YOUR NAME HERE
+ *  @author Tiffany Lee
  */
 public class TentConfig implements Configuration, ITentsAndTreesTest {
     /** square dimension of field */
     private static int DIM;
+    /** amount of tents in each row */
     private static ArrayList<Integer> rowTents;
+    /** amount of tents in each column */
     private static ArrayList<Integer> columnTents;
+    /** grid of cells containing empty(.), grass(-), tree(%), or tent(^) */
     private char[][] grid;
-    private int cursorRow = 0;
-    private int cursorColumn = -1;
+    /** cursor's row position */
+    private int cursorRow;
+    /** cursor's column position */
+    private int cursorColumn;
 
     /**
      * Construct the initial configuration from an input file whose contents
@@ -41,13 +46,13 @@ public class TentConfig implements Configuration, ITentsAndTreesTest {
         try (BufferedReader in = new BufferedReader(new FileReader(filename))) {
             // get the field dimension
             DIM = Integer.parseInt(in.readLine());
-            // get rows
+            // get tent amount in each row
             String[] rowVals = in.readLine().split("\\s+");
             rowTents = new ArrayList<>();
             for(String value : rowVals){
                 rowTents.add(Integer.parseInt(value));
             }
-            // get columns
+            // get tent amount in each column
             String[] columnVals = in.readLine().split("\\s+");
             columnTents = new ArrayList<>();
             for(String value : columnVals){
@@ -63,67 +68,141 @@ public class TentConfig implements Configuration, ITentsAndTreesTest {
                     }
                 }
             }
+            this.cursorRow = 0;
+            this.cursorColumn = -1;
         } // <3 Jim
     }
 
     /**
-     * Copy constructor.  Takes a config, other, and makes a full "deep" copy
+     * Copy constructor. Takes a config, other, and makes a full "deep" copy
      * of its instance data.
      * @param other the config to copy
      */
     private TentConfig(TentConfig other) {
-        // TODO
+        if (other.getCursorCol() + 1 < DIM) {
+            this.cursorRow = other.getCursorRow();
+            this.cursorColumn = other.getCursorCol() + 1;
+        } else if(other.getCursorCol() + 1 >= DIM && other.getCursorRow() + 1 < DIM){
+            this.cursorRow = other.getCursorRow() + 1;
+            this.cursorColumn = 0;
+        }
+        this.grid = new char[other.getDIM()][other.getDIM()];
+        for(int r = 0; r < DIM; r++){
+            System.arraycopy(other.grid[r], 0, this.grid[r], 0, DIM);
+        }
     }
 
+    /**
+     * Get the collection of successors from the current one
+     *
+     * @return All successors, valid and invalid
+     */
     @Override
     public Collection<Configuration> getSuccessors() {
-        // TODO
-        return new ArrayList<>();
+        Collection<Configuration> successorList = new ArrayList<>();
+        TentConfig s1 = new TentConfig(this);
+        TentConfig s2 = new TentConfig(this);
+        if(s1.grid[s1.cursorRow][s1.cursorColumn] == TREE){
+            successorList.add(s1);
+        } else if(s1.grid[s1.cursorRow][s1.cursorColumn] == EMPTY){
+            s1.grid[s1.cursorRow][s1.cursorColumn] = GRASS;
+            s2.grid[s2.cursorRow][s2.cursorColumn] = TENT;
+            successorList.add(s1);
+            successorList.add(s2);
+        }
+        return successorList;
     }
 
+    /**
+     * Is the current configuration valid or not?
+     *
+     * @return true if valid, false otherwise
+     */
     @Override
     public boolean isValid() {
         // TODO
         return true;
     }
 
+    /**
+     * Is the current configuration a goal?
+     *
+     * @return true if goal, false otherwise
+     */
     @Override
     public boolean isGoal() {
         // TODO
         return false;
     }
 
+    /**
+     * Displays a string representation of the grid to standard output.
+     *
+     * @return string representation of the grid to standard output
+     */
     @Override
     public String toString() {
         return getDisplay();
     }
 
-
+    /**
+     * Get the square dimension of the puzzle
+     *
+     * @return square dimension
+     */
     @Override
     public int getDIM() {
         return DIM;
     }
 
+    /**
+     * Get the number of tents for a row
+     *
+     * @param row the row
+     * @return number of tents
+     */
     @Override
     public int getTentsRow(int row) {
         return rowTents.get(row);
     }
 
+    /**
+     * Get the number of tents for a column
+     * @param col the column
+     * @return number of tents
+     */
     @Override
     public int getTentsCol(int col) {
         return columnTents.get(col);
     }
 
+    /**
+     * Get the contents at a cell
+     *
+     * @param row the row
+     * @param col the column
+     * @return the contents
+     */
     @Override
     public char getCell(int row, int col) {
         return grid[row][col];
     }
 
+    /**
+     * Get the cursor row location
+     *
+     * @return cursor row
+     */
     @Override
     public int getCursorRow() {
         return cursorRow;
     }
 
+    /**
+     * Get the cursor column location
+     *
+     * @return cursor column
+     */
     @Override
     public int getCursorCol() {
         return cursorColumn;
